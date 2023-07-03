da64c2df3335ba14bed00e8a0618c4ecb649b766
mateo <mateopolicichio@gmail.com>
2023-07-03 20:18:49 -0300

    actualizacion: paginas

--- civico/views.py
+++ civico/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'civico/index.html')
+
+def servicios(request):
+    return render(request, 'civico/servicios.html')
+
+def promociones(request):
+    return render(request, 'civico/promociones.html')
+
+def nosotros(request):
+    return render(request, 'civico/nosotros.html')
+
+def contacto(request):
+    return render(request, 'civico/contacto.html')
